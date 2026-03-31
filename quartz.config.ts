f7f63aa142e71e53fe239c25cc4418cf5c526290
itsbaam <cnmferreira18@gmail.com>
2026-03-30 23:11:32 -0300

    Add GitHub Actions deploy workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "garden.example.com",
+    baseUrl: "itsbaam.github.io/garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
